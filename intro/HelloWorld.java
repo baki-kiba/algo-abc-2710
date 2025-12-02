9ba70e67c061afb48ad22bfc60ac70bd737a211c
baki-kiba <karabadjabilel@gmail.com>
2025-12-02 16:29:52 +0100

    Hello World

--- intro/HelloWorld.java
+++ intro/HelloWorld.java
@@ -4,6 +4,6 @@ package intro;
 public class HelloWorld {
 
     public static void main(String[] args){
-        System.out.println("hello word");
+        System.out.println("hello word !");
     }
 }
